79e1e7a9fd79766736822c804c599c36a6a486b9
lgbao123 <luugiabao2609@gmail.com>
2023-10-13 11:19:20 -0700

    ingest,validate df

--- driver.py
+++ driver.py
@@ -1,6 +1,7 @@
 import get_env_variables as gev
 from create_spark import get_spark_object
 from validate import get_current_date
+from ingest import load_file,display_df,validate_df
 import logging
 import logging.config
 
@@ -14,6 +15,13 @@ def main():
         spark =get_spark_object(gev.envn,gev.appName)
         logging.info('Validate spark object')
         get_current_date(spark)
+        logging.info('Load file ')
+        # df1= load_file(spark,folder='olap')
+        df2 = load_file(spark,folder='oltp')
+        logging.info('Show dataframe :')
+        display_df(df2)
+        logging.info('Validate dataframe')
+        validate_df(df2)
     except Exception as e:
         logging.error(f'An error in main() : {str(e)}')
 
---
+++ ingest.py
@@ -0,0 +1,56 @@
+import logging.config
+import get_env_variables as gev
+import os
+logging.config.fileConfig('properties/configuration/logging.conf')
+logger = logging.getLogger('ingest')
+
+def get_info_file(file ,src):
+    file_dir = src + '/'+ file
+    if file.endswith('.parquet'):
+        format= 'parquet'
+        header ='NA'
+        inferSchema = 'NA'
+    elif file.endswith('.csv'):
+        format= 'csv'
+        header =gev.header
+        inferSchema = gev.inferSchema
+    return file_dir,format,header,inferSchema
+
+def load_file(spark ,folder):
+    try :
+        logger.warning('Start load file method.')
+        if folder  =='olap':
+            src = gev.source_olap
+        else:
+            src =gev.source_oltp
+
+        for file in os.listdir(src):
+            file_dir,format,header,inferSchema = get_info_file(file,src)
+            if format == 'parquet':
+                df = spark.read.format(format).load(file_dir)
+            elif format == 'csv':
+                df =spark.read.format(format).options(header=header).options(inferSchema=inferSchema).load(file_dir)
+    except Exception as e:
+        logger.error(f'An error in occurs in load_file(): {str(e)}')
+
+        raise
+    else:
+        logger.warning(f'Load file done which is of {format}..........')
+    
+    return df
+
+def display_df(df):
+    try:
+        return df.show()
+    except Exception as e:
+        logger.error(f'An error in occurs in display_df(): {str(e)}')
+        raise
+
+def validate_df(df):
+    logger.warning('Start validate df method')
+    try:
+        df_count = df.count()
+    except Exception as e:
+        logger.error(f'An enrror occurs validate_df(): {str(e)}')
+    else:
+        logger.warning(f'Number record df: {df_count}')
